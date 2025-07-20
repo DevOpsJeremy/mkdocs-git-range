8f50f38a4fff24883834aba396ea40cd901a841c
DevOps Jeremy <DevOpsJeremy@gmail.com>
2025-07-20 12:42:39 -0700

    Initial scaffolding

---
+++ src/mkdocs_git_range/__init__.py
@@ -0,0 +1,2 @@
+# Enter the version here if not using env $VERSION
+__version__ = '0.0.1'
---
+++ src/mkdocs_git_range/plugin.py
@@ -0,0 +1,7 @@
+from mkdocs.plugins import BasePlugin
+
+class GitRange(BasePlugin):
+    """
+    A MkDocs plugin to display the git range of the current documentation.
+    """
+
